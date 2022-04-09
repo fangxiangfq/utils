343724331fe79c0f08c649b1f65771a4c1350505
fangxiang <1627102210@qq.com>
2022-04-09 16:12:30 +0800

    char

---
+++ utils/str.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "namespace_def.h"
+
+Utils_Beg
+
+static inline bool low_case(char ch)
+{
+	return ch >= 'a' && ch <= 'z';
+}
+
+static inline bool up_case(char ch)
+{
+	return ch >= 'A' && ch <= 'Z';
+}
+
+static inline bool number(char ch)
+{
+	return ch >= '0' && ch <= '9';
+}
+
+static inline bool toupper(char ch)
+{
+	return (ch >= 'a' && ch <= 'z') ? (ch & ~0x20) : ch;
+}
+
+static inline bool tolower(char ch)
+{
+	return (ch >= 'A' && ch <= 'Z') ? (ch | 0x20) : ch;
+}
+
+Utils_End
